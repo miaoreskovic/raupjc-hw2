57003a65f55a6d2bff00d9c2177b7e4a8e8a1d45
Mia Orešković <mia.oreskovic92@gmail.com>
2017-11-05 23:58:21 +0100

    Added 6_zadatak

---
+++ 6_zadatak/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_zadatak
+{
+    public class Class1
+    {
+        static async Task<int> FactorialDigitSum(int n)
+        {
+
+            int sum = 1;
+            for (int i = n; i >= 0; i--)
+            {
+                sum *= i;
+            }
+            return sum;
+        }
+
+        private static async void AsyncFunctionCall()
+        {
+            Task<int> returnedTaskResult = FactorialDigitSum(5);
+            int sumResult = await returnedTaskResult;
+            Console.WriteLine(sumResult);
+        }
+
+        static void Main(string[] args)
+        {
+            AsyncFunctionCall();
+        }
+    }
+}
